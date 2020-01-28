9c1437ab23d522d50276fc01b0f28e3c3cb941d5
Diana Ponce <dianap2341@gmail.com>
2020-01-28 23:51:42 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,12 +1,12 @@
 def run_guessing_game
   random= rand(6) + 1
   user_input = gets.chomp
-  if user_input == random
-    puts "You guessed the correct number!"
+  if user_input == "exit"
+    puts "You Goodbye the correct number!"
   elsif user_input != random 
     puts "Sorry! The computer guessed #{random}."
-  elsif user_input == "exit"
-    puts "Goodbye!"
+  elsif user_input == random
+    puts "You guessed the correct number!"
   end
   
 end
