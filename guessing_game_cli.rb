ff589f378c9fa79936f2dd999e66d2d5e9f99b66
Diana Ponce <dianap2341@gmail.com>
2020-01-28 23:41:38 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,5 +2,7 @@ def run_guessing_game
   random= rand(6) + 1
   user_input = random.chomp
   if user_input == random
-    puts 
+    putsYou guessed the correct number!
+  else user_input !== random 
+    
 end
