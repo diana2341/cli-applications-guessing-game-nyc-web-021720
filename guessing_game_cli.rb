d1d897de697428dca334f5bd8e4ddc4b6254d5ae
Diana Ponce <dianap2341@gmail.com>
2020-01-28 23:44:13 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   user_input = random.chomp
   if user_input == random
     puts "You guessed the correct number!"
-  elsif user_input !== random 
+  elsif user_input != random 
     puts "Sorry! The computer guessed #{random}"
   else user_input == "exit"
     puts "goodbye!"
